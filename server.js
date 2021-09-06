bd923c7a08944c711f65e6c4df703c7b113fc4b8
olegs7771 <olegs7771@gmail.com>
2021-09-05 20:44:52 -0500

    ok

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const nodesTreeRouter = require('./routes/nodesTree');
 app.use(express.json({ limit: '10kb' }));
 
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+//Public Folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.setHeader('set-cookie', ['SameSite=Strict;SameSite=Strict']);
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use('/api/v1/nodes', nodesTreeRouter);
 
